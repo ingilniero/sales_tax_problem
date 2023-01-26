ed14379614a1a6a4d03f53b2e1b7160cfc442ab7
Gilberto Villa <ingilniero@gmail.com>
2023-01-26 15:01:34 -0600

    Fix decimal rounding

--- lib/tax_calculator.rb
+++ lib/tax_calculator.rb
@@ -7,7 +7,7 @@ class TaxCalculator
 
     subtotal = price + sale_tax(product)
     subtotal += import_duty_tax(product)
-    subtotal.floor(2)
+    subtotal.round(2)
   end
 
   private
@@ -15,13 +15,13 @@ class TaxCalculator
   def sale_tax(product)
     return 0 if excluded_product_type?(product.type)
 
-    (product.price * BASE_SALE_TAX).floor(2)
+    (((product.price * BASE_SALE_TAX).round(2) * 10).ceil) / 10.0
   end
 
   def import_duty_tax(product)
     return 0 unless product.imported?
 
-    (product.price * IMPORT_DUTY_TAX).floor(2)
+    (((product.price * IMPORT_DUTY_TAX).round(2) * 10).ceil) / 10.0
   end
 
   def excluded_product_type?(type)
--- test/tax_calculator_test.rb
+++ test/tax_calculator_test.rb
@@ -15,13 +15,22 @@ class TaxCalculatorTest < Minitest::Test
     assert_equal 11, sale_tax
   end
 
-  def test_basic_sale_tax_for_all_goods
-    product = Product.new(price: 12.49)
+  def test_sale_tax_get_rounded_to_the_nearest_0_05
+    product = Product.new(price: 10)
 
     calculator = TaxCalculator.new
     sale_tax = calculator.calculate_sale_tax(product)
 
-    assert_equal 13.73, sale_tax
+    assert_equal 11, sale_tax
+  end
+
+  def test_basic_sale_tax_for_all_goods
+    product = Product.new(price: 14.99)
+
+    calculator = TaxCalculator.new
+    sale_tax = calculator.calculate_sale_tax(product)
+
+    assert_equal 16.49, sale_tax
   end
 
   def test_no_sale_tax_for_books
@@ -57,15 +66,15 @@ class TaxCalculatorTest < Minitest::Test
     calculator = TaxCalculator.new
     sale_tax = calculator.calculate_sale_tax(product)
 
-    assert_equal 32.16, sale_tax
+    assert_equal 32.19, sale_tax
   end
 
   def test_import_duty_tax_for_food
-    product = Product.new(name: "imported box of chocolates", price: 10.00)
+    product = Product.new(name: "imported box of chocolates", price: 11.25)
 
     calculator = TaxCalculator.new
     sale_tax = calculator.calculate_sale_tax(product)
 
-    assert_equal 10.5, sale_tax
+    assert_equal 11.85, sale_tax
   end
 end
